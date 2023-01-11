939ae21d6360701905edd796ef5ab6669f0df2ea
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-10 22:13:04 -0400

    Update

---
+++ node-06/globalThree.js
@@ -0,0 +1,10 @@
+/* Exemplo 6 - Global Object */
+
+console.clear();
+console.log(__dirname);
+console.log(__filename);
+
+
+
+
+
---
+++ node-06/globalTwo.js
@@ -0,0 +1,11 @@
+/* Exemplo 6 - Global Object */
+
+console.clear();
+console.log('teste!');
+setTimeout(() => {
+    console.log('Terminado!');
+}, 3000); //passa 3 segundo e executa (terminado)
+
+
+
+
