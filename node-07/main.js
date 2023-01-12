eb6b5a79427da0653f34df1309f531e1e9007735
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-12 08:35:06 -0400

    Update

---
+++ node-07/main.js
@@ -0,0 +1,26 @@
+/* módulos e require */
+console.log('Inicio');
+
+//incluindo o modulo
+//let pessoas = require('./modulo');
+//console.log(pessoas.nomes);
+
+//const texto = require("./modulo");
+//console.log(texto);
+
+const pessoas = require('./modulo');
+console.log(pessoas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ node-07/modulo.js
@@ -0,0 +1,9 @@
+let nomes = ['daniel', 'priscila', 'sapup3'];
+//console.log('Estou em modulo.js');
+//console.log(nomes);
+
+//module.exports = "Esta frase está no modulo.js";
+module.exports = nomes
+
+
+
