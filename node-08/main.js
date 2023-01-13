e6aaa53536ab86db8ad6179891ef4cb0addd011b
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-13 09:21:42 -0400

    Update

--- node-08/main.js
+++ node-08/main.js
@@ -1,12 +1,14 @@
-/* ======= Exemplo 1 ======= */
-
+/* ======= Exemplo 1 - Desestruturação ======= */
 console.log('Inicio');
 
-const dados = require('./modulo');
-console.log(dados);
+//const { nomes } = require('./modulo');
+//console.log(nomes);
 
-
-
+const { nomes, cidades, texto, adicionar } = require('./modulo');
+console.log(nomes);
+console.log(cidades);
+console.log(texto);
+console.log(adicionar(10, 20));
 
 
 
--- node-08/modulo.js
+++ node-08/modulo.js
@@ -1,11 +1,18 @@
 /* ======= Exemplo 1 ======= */
 const nomes = ['Sapup3', 'Daniel', 'Priscila'];
 const cidades = ['Manaus', 'São Paulo', 'Rio de Janeiro'];
+const texto = "Olá mundo! Sapup3 na área!";
 
+
+function adicionar(a, b){
+    return a + b;
+}
 
 module.exports = {
     nomes,
     cidades,
+    texto,
+    adicionar,
 };
 
 
