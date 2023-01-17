efa1aeacc5bb687114568853a575ded7601ffc4e
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-17 19:05:18 -0400

    Create project node-11

---
+++ node-11/app.js
@@ -0,0 +1,29 @@
+/* NodeJS módulo File System */
+
+const fs = require('fs');
+
+//reading files
+if(fs.existsSync('./dados.csv')){
+    fs.readFile('./dados.csv', (err, data) => {
+        if(err){
+            console.log(err);
+        }else {
+            console.log(data.toString());
+        }
+    });
+}else {
+    console.log('Ficheiro não encontrado!');
+}
+
+//aparece primeiro (readfile)
+console.log('Ultima operação');
+
+
+
+
+
+
+
+
+
+
