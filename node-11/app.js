690e3dbbf04bde6f3c9560f5c6e6dcd706d207c3
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-17 23:54:48 -0400

    Update

--- node-11/app.js
+++ node-11/app.js
@@ -3,20 +3,29 @@
 const fs = require('fs');
 
 //write file, 3*(ordem de visualização)
-fs.writeFile('./dados1.txt', 'Daniel Oliveira', () => {
-    console.log('Ficheiro escrito!');
-});
+//fs.writeFile('./dados1.txt', 'Daniel Oliveira', () => {
+//    console.log('Ficheiro escrito!');
+//});
 
 //write file Sync, 1*(ordem de visualização)
-fs.writeFileSync('./dados2.txt', 'Priscila Andrade');
+//fs.writeFileSync('./dados2.txt', 'Priscila Andrade');
+
+//adicionando conteudo
+//Write file - append
+//fs.appendFile('./dados1.txt', 'PUBG\n', (err) => {
+//    console.log(err)
+//});
+
+fs.appendFileSync('./dados1.txt', 'NodeJS\n');
 
 //aparece primeiro, 2*(ordem de visualização) 
 console.log('Ultima operação');
 
 
 
-
-
+/*Notas
+Sync = bloqueando o código
+*/
 
 
 
