4e87068322372b522444db2903b8dc5e26603838
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-19 08:49:32 -0400

    Create project node-12

--- node-12/app.js
+++ node-12/app.js
@@ -4,10 +4,7 @@ const fs = require('fs');
 
 
 
-fs.appendFileSync('./dados1.txt', 'NodeJS\n');
-
-//aparece primeiro, 2*(ordem de visualização) 
-console.log('Ultima operação');
+//create folder
 
 
 
