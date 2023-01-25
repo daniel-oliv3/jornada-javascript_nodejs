b6d1e458ae0a6385173fd382095f10c0aa340431
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-25 18:20:00 -0400

    Update

--- node-17/serverDois.js
+++ node-17/serverDois.js
@@ -1,12 +1,15 @@
-/* CLIENTE E SERVIDOR 1 */
+/* CLIENTE E SERVIDOR 2 */
 
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    //Header
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<h3>teste</h3>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
     console.log('Servidor iniciado com sucesso!');
 });
-
