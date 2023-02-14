07e0a4f4581794e996115e91315a456ce02a4818
Daniel Oliveira <danielsapup3@gmail.com>
2023-02-14 09:19:16 -0400

    Create project node-27

---
+++ node-27/app.js
@@ -0,0 +1,37 @@
+/* */
+const express = require('express');
+
+//cria uma aplicação express
+const app = express();
+
+//escutar os requests
+app.listen(3000);
+
+//routes
+app.get('/', (req, res) => {
+    //res.sendFile('./views/home.html');
+    //res.sendFile('C:/Users/Daniel_Oliveira/Documents/GitHub/jornada-javascript_nodejs/node-26/views/home.html');
+    res.sendFile('./views/home.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    //res.sendFile('./views/about.html');
+    res.sendFile('./views/about.html', { root: __dirname });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
