9e6a437481f4afa97ba0ba9ca29afb1d62d140b3
Daniel Oliveira <danielsapup3@gmail.com>
2023-02-21 18:57:57 -0400

    REDIRECTS

--- node-21/server.js
+++ node-21/server.js
@@ -43,3 +43,20 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
     console.log('Servidor iniciado com sucesso!');
 });
+
+
+
+
+
+
+
+
+
+
+
+/*
+    Autor: Daniel Oliveira
+    Email: danieloliveira.webmaster@gmail.com
+    Manaus/Amazonas/Brasil
+    21/02/2023
+*/
