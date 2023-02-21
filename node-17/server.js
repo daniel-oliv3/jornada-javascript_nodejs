bfa96c8b1d35f45850b3ae6130980abe06028c83
Daniel Oliveira <danielsapup3@gmail.com>
2023-02-21 18:55:06 -0400

    RELEMBRANDO AS RESPOSTAS

--- node-17/server.js
+++ node-17/server.js
@@ -10,3 +10,13 @@ server.listen(3000, 'localhost', () => {
     console.log('Servidor iniciado com sucesso!');
 });
 
+
+
+
+/*
+    Autor: Daniel Oliveira
+    Email: danieloliveira.webmaster@gmail.com
+    Manaus/Amazonas/Brasil
+    21/02/2023
+*/
+
